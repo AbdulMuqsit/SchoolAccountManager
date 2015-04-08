d0d62fae8b22f56bce9f756ba278ee288d32aeac
AMuqsit <abdemuqsit@hotmail.com>
2015-04-08 14:30:20 +0500

    Updat views, view models

--- SchoolAccountManager.WPF/Infrastructure/ViewModelLocator.cs
+++ SchoolAccountManager.WPF/Infrastructure/ViewModelLocator.cs
@@ -1,14 +1,37 @@
-﻿using SchoolAccountManager.WPF.ViewModel;
+﻿using System.Runtime.Serialization.Formatters;
+using SchoolAccountManager.WPF.ViewModel;
 
 namespace SchoolAccountManager.WPF.Infrastructure
 {
     public class ViewModelLocator
     {
-        public ViewModelBase MainViewModel { get; set; }
+        public MainViewModel MainViewModel { get; set; }
+        public HomeViewModel HomeViewModel { get; set; }
 
         public ViewModelLocator()
         {
             MainViewModel = new MainViewModel();
+            HomeViewModel = new HomeViewModel();
         }
+    }
+
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentChildViewModel
+        {
+            get
+            {
+                if (ViewModelLocator== null)
+                {
+                    return new HomeViewModel();
+                }
+                return ViewModelLocator.HomeViewModel;
+            }
+            set
+            {
+            }
+        }
+
+
     }
 }
--- SchoolAccountManager.WPF/ViewModel/HomeViewModel.cs
+++ SchoolAccountManager.WPF/ViewModel/HomeViewModel.cs
@@ -7,7 +7,7 @@ using SchoolAccountManager.WPF.Infrastructure;
 
 namespace SchoolAccountManager.WPF.ViewModel
 {
-    public class MainViewModel:ViewModelBase
+    public class HomeViewModel:ViewModelBase
     {
         public RelayCommand PaymentButtonCommand { get; private set; }
         public RelayCommand InvoiceButtonCommand { get; private set; }
