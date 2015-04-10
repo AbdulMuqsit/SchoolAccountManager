aa319b84317b8620390aec19c8f8b4f6708b266a
AMuqsit <abdemuqsit@hotmail.com>
2015-04-10 16:49:40 +0500

    Release offline

--- SchoolAccountManager.WPF/View/HomeView.xaml.cs
+++ SchoolAccountManager.WPF/View/HomeView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace SchoolAccountManager.WPF.View
 {
@@ -10,6 +12,11 @@ namespace SchoolAccountManager.WPF.View
         public HomeView()
         {
             InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
