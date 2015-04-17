a48ac6b7a8551f77adebbf4e005627f63247eaaf
AMuqsit <abdemuqsit@hotmail.com>
2015-04-17 14:40:01 +0500

    fixed currency symbol

--- SchoolAccountManager.WPF/App.xaml.cs
+++ SchoolAccountManager.WPF/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
 
 namespace SchoolAccountManager.WPF
 {
@@ -7,5 +10,13 @@ namespace SchoolAccountManager.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ig-NG");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ig-NG");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ig-NG");
+            base.OnStartup(e);
+        }
     }
 }
